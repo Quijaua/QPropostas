0f42ae613e4d8c184ac793b81d4f24a8b4473811
quijaua <folhas@quijaua.com.br>
2015-01-28 21:47:39 -0200

    arquivo inicial propostas.php

---
+++ propostas.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is responsible for starting the plugin using the main plugin
+ * class file.
+ *
+ * @package           Propostas
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Propostas
+ * Plugin URI:        http://opensource.quijaua.com.br
+ * Description:       Plugin que gerencia propostas
+ * Version:           0.1.0
+ * Author:            Rafael Bantu, Eduardo Alencar
+ * Author URI:        http://github.com/Quijaua/propostas
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       propostas
+ */
+
+if( !defined('PLUGIN_DIR_PATH') ) {
+    define('PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
+}
+
+if( !defined('PLUGIN_URL') ) {
+    define('PLUGIN_URL', plugins_url() . '/propostas/' );
+}
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+require_once PLUGIN_DIR_PATH . 'admin/class-propostas-custom-post.php';
+
+require_once PLUGIN_DIR_PATH . 'admin/class-propostas-taxonomy.php';
+
+require_once PLUGIN_DIR_PATH . 'admin/class-propostas-meta-box.php';
+
+require_once PLUGIN_DIR_PATH . 'admin/class-settings-page.php';
+
+require_once PLUGIN_DIR_PATH . 'admin/class-propostas.php';
+
+
+
+
+function run_propostas() {
+    $propostas = new Propostas_Admin( 'propostas', '0.3.0' );
+}
+run_propostas();
