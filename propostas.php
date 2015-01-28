126a427a685eedaf17849ab27ae33d171b751364
quijaua <folhas@quijaua.com.br>
2015-01-28 21:49:39 -0200

    ajusta na licença

--- propostas.php
+++ propostas.php
@@ -15,8 +15,8 @@
  * Version:           0.1.0
  * Author:            Rafael Bantu, Eduardo Alencar
  * Author URI:        http://github.com/Quijaua/propostas
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain:       propostas
  */
 
